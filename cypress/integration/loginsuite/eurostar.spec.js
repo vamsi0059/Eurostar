73853d8553e21438bee84bcb8f91055f669a23a2
vamsi_cypress <vamsikrish0059@gmail.com>
2023-08-01 10:54:50 +0530

    adding eurostar code for clint

--- cypress/integration/loginsuite/eurostar.spec.js
+++ cypress/integration/loginsuite/eurostar.spec.js
@@ -1,47 +1,32 @@
 import { loginpage } from "../pages/eurostar";
 
 
-describe("Testing the railway ticket booking system", ()=>{
+describe("Testing the railway ticket booking system", () => {
     let loginuserdata;
     before(() => {
         cy.fixture('login.json').then((userdata) => ((loginuserdata) = (userdata)))
     })
-    it("Built the project for railway ticket booking",() =>{
+    it("Built the project for railway ticket booking", () => {
         cy.visit(Cypress.env('baseUrl'));
-        loginpage.aceptAllCache();
-        loginpage.clickThetrainsButton()
-        loginpage.clickTheReturncheckBox()
+        loginpage.aceptAllCache(loginuserdata.validationdetails);
+        loginpage.clickThetrainsButton(loginuserdata.validationdetails);
+        loginpage.clickTheReturncheckBox(loginuserdata.validationdetails);
         loginpage.enterTheFromDetails(loginuserdata.tiketDetails);
-        loginpage.enterTheToDetails(loginuserdata.tiketDetails)
-        loginpage.clickTheDateText()
-        loginpage.clickTheDateTimeText()
-        loginpage.clickTheDateButton()
-        loginpage.thePassengerDetails()
-        loginpage.theAdultsPassengerDetails()
-        loginpage.adultsDetailsOkButton()
-        loginpage.clickTheSearchButton()
-        loginpage.selectOutBoundDetails()
-        loginpage.clickTheOutboundSubmited()
-        loginpage.selectReturnDetails()
-        loginpage.clickTheReturnSubmited()
-        loginpage.clickContinueButton()
-        loginpage.checkOutAsGueast()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        loginpage.enterTheToDetails(loginuserdata.tiketDetails);
+        loginpage.clickTheDateText(loginuserdata.tiketDetails);
+        loginpage.clickTheDateTimeText();
+        loginpage.clickTheDateButton(); 
+        loginpage.thePassengerDetails();
+        loginpage.theAdultsPassengerDetails();
+        loginpage.adultsDetailsOkButton();
+        loginpage.clickTheSearchButton(loginuserdata.tiketDetails);
+        loginpage.selectOutBoundDetails();
+        loginpage.clickTheOutboundSubmited();
+        loginpage.selectReturnDetails();
+        loginpage.clickTheReturnSubmited();
+        loginpage.clickContinueButton();
+        loginpage.checkOutAsGueast();
+        loginpage.checkOutAsScreanText(loginuserdata.validationdetails);
 
     })
 
